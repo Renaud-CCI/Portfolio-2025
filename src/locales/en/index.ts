059831bb465d9741cc83d4bd2e9eaf2cf134b57b
RenaudBresson <120473987+Renaud-CCI@users.noreply.github.com>
2025-06-19 14:23:41 +0200

    Portfolio - adding english translation

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,9 +1,11 @@
 import about from './about.json' assert { type: 'json' }
 import home from './home.json' assert { type: 'json' }
 import nav from './nav.json' assert { type: 'json' }
+import portfolio from './portfolio.json' assert { type: 'json' }
 
 export default {
   about,
   home,
   nav,
+  portfolio,
 }
